98444745047c5e029d50efb60a18aaeb21b38c3d
Inna <forincha@gmail.com>
2024-01-13 19:57:32 +0200

    REQ-07: Use enum for dog size

--- src/main/java/com/magicvet/SandBox.java
+++ src/main/java/com/magicvet/SandBox.java
@@ -1,12 +1,11 @@
 package main.java.com.magicvet;
 
-import main.java.com.magicvet.comparator.DogSizeComparator;
-import main.java.com.magicvet.comparator.PetAgeComparator;
 import main.java.com.magicvet.model.Dog;
 import main.java.com.magicvet.model.Cat;
 import main.java.com.magicvet.model.Pet;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SandBox {
     public static void main(String[] args) {
@@ -26,16 +25,21 @@ class PetRepository {
     static void testDogCompareSize_shouldBeOrderByAscending() {
         //ARRANGE
         Dog[] dogs = {
-                new Dog(Dog.M),
-                new Dog(Dog.S),
-                new Dog(Dog.XL),
-                new Dog(Dog.XL),
-                new Dog(Dog.XS),
-                new Dog(Dog.S)
+                new Dog(Dog.Size.M),
+                new Dog(Dog.Size.S),
+                new Dog(Dog.Size.XL),
+                new Dog(Dog.Size.XL),
+                new Dog(Dog.Size.XS),
+                new Dog(Dog.Size.S)
         };
 
         //ACT
-        Arrays.sort(dogs, new DogSizeComparator());
+        Arrays.sort(dogs, new Comparator<Dog>() {
+            @Override
+            public int compare(Dog dog1, Dog dog2) {
+                return dog1.getSize().getValue() - dog2.getSize().getValue();
+            }
+        });
 
         //ASSERT
         for (Dog dog : dogs) {
@@ -53,7 +57,12 @@ class PetRepository {
         };
 
         //ACT
-        Arrays.sort(cats, new PetAgeComparator());
+        Arrays.sort(cats, new Comparator<Pet>() {
+            @Override
+            public int compare(Pet pet1, Pet pet2) {
+                return Integer.parseInt(pet1.getAge()) - Integer.parseInt(pet2.getAge());
+            }
+        });
 
         //ASSERT
         for (Pet cat : cats) {
